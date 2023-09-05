994f67de8fad4026e421f152ac0fafdeedcb17f0
lorevis92 <lorenzo.visconti@hotmail.it>
2023-09-05 11:41:09 +0200

    Implementazione progetto con settaggi di base (per Collegare il DB all'API)

---
+++ src/main/java/BoulderKing/EsercitazioneBeSettimana7Giorno1Application.java
@@ -0,0 +1,13 @@
+package BoulderKing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercitazioneBeSettimana7Giorno1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercitazioneBeSettimana7Giorno1Application.class, args);
+	}
+
+}
---
+++ src/test/java/BoulderKing/BoulderKingApplicationTests.java
@@ -0,0 +1,13 @@
+package BoulderKing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoulderKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
