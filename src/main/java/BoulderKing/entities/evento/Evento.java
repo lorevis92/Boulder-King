ca75dddc1662ccc69fe52b9edc7d32ee9430c421
lorevis92 <lorenzo.visconti@hotmail.it>
2023-10-06 10:01:30 +0200

    Corretto bug nel cambio informazioni Evento

--- src/main/java/BoulderKing/entities/evento/Evento.java
+++ src/main/java/BoulderKing/entities/evento/Evento.java
@@ -50,6 +50,7 @@ public class Evento {
 	private String provincia;
 	private String citta;
 	private String zonaItalia;
+	@Column(length = 100000)
 	private String info;
 	@Column(length = 100000)
 	private String sito;
